80029c774335d016da745ac3e6a616ad84cebd74
robin chua <robinchua@gmail.com>
2017-05-01 11:57:48 +0800

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+  get '/about' => 'pages#about'
+  get '/comuters' =>'pages#view_commuters'
+  get '/contact' => 'pages#contact_us'
+  get '/profile' => 'pages#profile'
+  get '/dashboard' => 'pages#dashboard'
+  get '/matches' => 'pages#matches'
+  get '/login' => 'pages#login'
+  get '/signup' => 'pages#signup'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
