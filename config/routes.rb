03375224d81146e2b0477e8e1ffa97e6fcd44f63
hexhex23 <mariawilson23@gmail.com>
2017-05-02 23:52:35 +0800

    devise controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
     root 'pages#home'
     get '/about' => 'pages#about'
     # post '/about' => update_attributes(params[:post])
