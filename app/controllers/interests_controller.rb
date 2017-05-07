6134a300117f79378fd5ee21c64e8464ada7ecc5
Felix Tan <felixtanhm@gmail.com>
2017-05-07 12:52:32 +0800

    added filter in controller

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,47 +1,47 @@
 class InterestsController < ApplicationController
-  def index
-    @all_interests = Interest.all
-    gon.interests = Interest.all
-  end
-
-  def show
-    @individual_interest = Interest.find(params[:id])
-    gon.interest = Interest.find(params[:id])
-  end
-
-  def edit
-    @individual_interest = Interest.find(params[:id])
-  end
-
-  def new
-    @new_interest = Interest.new
-  end
-
-  def create
-    @submitted_interest = params[:interest]
-    @submitted_interest = Interest.new(filter_params)
-    @submitted_interest.save
-    redirect_to profile_path
-  end
-
-  def update
-    @updated_interest = Interest.find(params[:id])
-    if @updated_interest.update(filter_params)
-      redirect_to root_path
+    def index
+        if params[:description] && Interest.all.collect(&:description).include?(params[:area][:description])
+            @all_interests = Interest.send(params[:area][:description].downcase)
+        else
+            @all_interests = Interest.all
+            gon.interests = Interest.all
+        end
     end
-  end
 
-  def destroy
-    @deleted_interest = Interest.find(params[:id])
-    @deleted_interest.destroy
-    redirect_to root_path
-  end
+    def show
+        @individual_interest = Interest.find(params[:id])
+        gon.interest = Interest.find(params[:id])
+    end
 
-  private
+    def edit
+        @individual_interest = Interest.find(params[:id])
+    end
 
-  def filter_params
-    params.require(:interest).permit(:category, :description)
-  end
+    def new
+        @new_interest = Interest.new
+    end
 
+    def create
+        @submitted_interest = params[:interest]
+        @submitted_interest = Interest.new(filter_params)
+        @submitted_interest.save
+        redirect_to profile_path
+    end
 
+    def update
+        @updated_interest = Interest.find(params[:id])
+        redirect_to root_path if @updated_interest.update(filter_params)
+    end
+
+    def destroy
+        @deleted_interest = Interest.find(params[:id])
+        @deleted_interest.destroy
+        redirect_to root_path
+    end
+
+    private
+
+    def filter_params
+        params.require(:interest).permit(:category, :description)
+    end
 end
