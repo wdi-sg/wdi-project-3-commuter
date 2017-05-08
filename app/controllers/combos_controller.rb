417cc188175d85d3a1387f8e4bbdeb93ffc54de7
Felix Tan <felixtanhm@gmail.com>
2017-05-08 09:57:15 +0800

    added route filter

--- app/controllers/combos_controller.rb
+++ app/controllers/combos_controller.rb
@@ -1,28 +1,39 @@
 class CombosController < ApplicationController
-  def index
-  end
+    def index
+        if params[:interest] && Combo.all.collect(&:interest).include?(params[:area][:interest])
+            @all_interests = Combo.send(params[:area][:interest].downcase)
+        else
+            @all_interests = Combo.all
+            gon.interests = Combo.all
+        end
+    end
 
-  def show
-  end
+    def show
+        # find interest by id
+        @individual_interest = Combo.find(params[:id])
+        # pass the data into your js
+        gon.interest = Combo.find(params[:id])
+        # alert the data into the screen
+    end
 
-  def edit
-  end
+    def edit
+    end
 
-  def new
-  end
+    def new
+    end
 
-  def create
-  end
+    def create
+    end
 
-  def update
-  end
+    def update
+    end
 
-  def destroy
-  end
+    def destroy
+    end
 
-  private
+    private
 
-  def filter_params
-    params.require(:combo).permit(:user_id, :travel_id, :interest_id)
-  end
+    def filter_params
+        params.require(:combo).permit(:user_id, :travel_id, :interest_id)
+    end
 end
