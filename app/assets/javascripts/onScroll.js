019f1a54e1e2b0d6de6c99fde7b871976510df6b
arif rahman <matrep88@hotmail.com>
2017-05-09 00:20:26 +0800

    'styling'

--- app/assets/javascripts/onScroll.js
+++ app/assets/javascripts/onScroll.js
@@ -1,36 +1,31 @@
-// $(document).on('ready page:load', function($) {
-
-// // Init Wow
-// wow = new WOW( {
-//     animateClass: 'animated',
-//     offset:       100
-// });
-// wow.init();
-
-// Navigation scrolls
-// $('.navbar-nav li a').bind('click', function(event) {
-//   $('.navbar-nav li').removeClass('active');
-//   $(this).closest('li').addClass('active');
-//   var $anchor = $(this);
-//   var nav = $($anchor.attr('href'));
-//   if (nav.length) {
-//     $('html, body').stop().animate({
-//       scrollTop: $($anchor.attr('href')).offset().top
-//     }, 1500, 'easeInOutExpo');
-//
-//     event.preventDefault();
-//   }
-// });
-
-
-//jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
+
+  var wScroll = $(this).scrollTop()
+
+  // collapse the navbar on scroll
   if ($(".navbar-default").offset().top > 50) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
+    $(".navbar-fixed-top").addClass("top-nav-collapse")
   } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    $(".navbar-fixed-top").removeClass("top-nav-collapse")
   }
 
-});
+  // team-member animation
+  if(wScroll > $('.team-row').offset().top - ($(window).height() / 1.2)) {
 
-// });
+    console.log("hi");
+
+    $('.team-row .team-col').each(function(i){
+
+      setTimeout(function(){
+        $('.team-row .team-col').eq(i).addClass('pop')
+      }, 150 * (i + 1))
+
+    })
+
+  }
+
+
+
+
+
+});
