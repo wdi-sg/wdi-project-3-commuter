18d2a9454cd590295b9cf3df6800be107afadd47
robin chua <robinchua@gmail.com>
2017-05-08 15:31:54 +0800

    chat is working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     root 'pages#home'
     get '/about' => 'pages#about'
     # post '/about' => update_attributes(params[:post])
+    get '/chat' => 'home#index', :as => 'chats'
+    # get '/chat/:id' => 'home#show', :as => 'chat'
     get '/commuters' => 'pages#view_commuters'
     get '/contact' => 'pages#contact_us'
     get '/profile' => 'pages#profile'
@@ -18,6 +20,12 @@ Rails.application.routes.draw do
     resources :travels
     resources :interests
     resources :combos
+    resources :conversations, only: [:create, :show] do
+    member do
+    post :close
+    end
+    resources :messages, only: [:create]
+  end
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
