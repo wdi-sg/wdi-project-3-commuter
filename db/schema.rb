f26a99b738a2fb715fdf6ad5511eaf6d50d435fb
hexhex23 <mariawilson23@gmail.com>
2017-05-08 17:33:15 +0800

    nav bar conflicts resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170508041325) do
 
-ActiveRecord::Schema.define(version: 20170508021710) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "combos", force: :cascade do |t|
     t.datetime "created_at",  null: false
