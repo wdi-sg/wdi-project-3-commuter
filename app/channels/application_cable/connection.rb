b18a766a6c93e9c18907b76a30d0b5dd704adae3
hexhex23 <mariawilson23@gmail.com>
2017-05-11 22:37:50 +0800

    fix action cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,19 +1,16 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
-
     def connect
-      self.current_user = find_verified_user
-    end
+         self.current_user = find_verified_user
+       end
 
-    protected
+       protected
 
-    def find_verified_user
-      if (current_user = env['warden'].user)
-        current_user
-      else
-        reject_unauthorized_connection
-      end
-    end
-  end
+       def find_verified_user
+         user = User.find_by_id(cookies.signed[:user_id])
+         return user if user
+         fail 'User needs to be authenticated'
+       end
+     end
 end
